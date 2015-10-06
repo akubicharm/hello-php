cd95997a2f20468db619341a3f17f7ff92978c0e
Kei Omizo <komizo@redhata.com>
2015-10-06 22:38:14 +0900

    external db

---
+++ set-env2dc.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+DC_NAME=`oc get dc |grep hello-php |awk '{print $1}'`
+
+OPTS="\
+-e MYSQL_USER=root  \
+-e MYSQL_PASSWORD=mysql  \
+-e MYSQL_DATABASE=mydb "
+
+echo "oc env dc $DC_NAME $OPTS"
+oc env dc $DC_NAME $OPTS
